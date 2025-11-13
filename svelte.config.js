7e72f10f8bfcd8fca5efcb78c8a4e3a1bafff3b3
Max Brenner <max@shipit.dev>
2025-11-13 20:57:19 +0100

    switch to cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		paths: {
-			base:  process.argv.includes('dev') ? '' : '/kubedex'
-		},
-		adapter: adapter(
-			{
-				fallback: 'index.html',
-				strict: true,
-				precompress: true
-			}
-		),
+		adapter: adapter(),
 	}
 };
 
